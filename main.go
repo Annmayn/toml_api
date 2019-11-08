6fe860b9c04b425fa8d66acd184836e144910a13
Annmayn <ntvirus333@gmail.com>
2019-11-08 14:52:32 +0545

    updated init to generate apiEndPoints

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"./initializer"
 )
 
 func main() {
 	resourceLocation := "resource.toml"
-	initializer.InitializeRoutes(resourceLocation)
+
+	//returns config, map of endpoint to resource and error
+	config, apiEndPoint, _ := initializer.InitializeRoutes(resourceLocation)
+	fmt.Println(config)
+	fmt.Println("........")
+	fmt.Println(apiEndPoint)
 }
