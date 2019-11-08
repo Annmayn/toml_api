bc319db156087fcc313002e1ba148f73fdb6fdd5
Annmayn <ntvirus333@gmail.com>
2019-11-08 09:56:44 +0545

    added code to load config to memmory

--- initializer/initialize.go
+++ initializer/initialize.go
@@ -1,31 +1,58 @@
 package initializer
 
 import (
-	"fmt"
+	"errors"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 )
 
-//InitializeRoutes : route initialization
-func InitializeRoutes(resourceLocation string) {
-	var r map[string]interface{}
-	if _, err := toml.DecodeFile(resourceLocation, &r); err != nil {
-		fmt.Println("Error")
-	} else {
-		// var apiEndPoint interface{}
-		var endPoints map[string]interface{}
-		//endPoints : root, detail, approve
-		endPoints = r["api"].(map[string]interface{})
-
-		for endPoint := range endPoints {
-			methods := endPoints[endPoint].(map[string]interface{})
-			if _, ok := methods["endpoint"]; !ok {
-				//error
-				fmt.Println("No endpoint defined")
-			} else {
-				//methods[1:] = [get, post,...]
-				fmt.Println("Handle all the handlers here")
-			}
+func evaluateURL(url string) int {
+	urlSplit := strings.Split(url, "/")
+	for ind, dir := range urlSplit {
+		if dir[0] == '{' {
+			return ind
 		}
 	}
+	return 0
+}
+
+//////////////////////////////////////////////////////////e
+//Can be used to check schema and redirect (not complete)
+//////////////////////////////////////////////////////////
+// var rInter interface{}
+// 	if _, err := toml.DecodeFile(resourceLocation, &rInter); err != nil {
+// 		fmt.Println(err)
+// 	} else {
+// 		r := rInter.(map[string]interface{})
+// 		var endPoints map[string]interface{}
+
+// 		endPoints = r["api"].(map[string]interface{}) //endPoints : root, detail, approve
+
+// 		for method := range endPoints {
+// 			var url string
+// 			if method == "endpoint" {
+// 				url, ok := endPoints[method].(string)
+// 				if !ok {
+// 					fmt.Println("Endpoint not defined")
+// 				} else {
+
+// 				}
+// 			} else {
+// 				//handle all validations and what not
+// 				//store schema in memory
+// 				r.HandleFunc()
+// 			}
+// 		}
+// 		// }
+// 	}
+////////////************************************///////////
+
+//InitializeRoutes : route initialization
+func InitializeRoutes(resourceLocation string) (map[string]interface{}, error) {
+	var configInterface interface{}
+	if _, err := toml.DecodeFile(resourceLocation, &configInterface); err != nil {
+		return make(map[string]interface{}), errors.New("Can't read from file")
+	}
+	return configInterface.(map[string]interface{}), nil
 }
