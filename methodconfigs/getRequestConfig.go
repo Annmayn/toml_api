0dc617be0d890b9a35cffe90d3f86aed5dceb7c2
Mandeep Khatry <mandeepkhatry1997@gmail.com>
2019-11-12 11:58:12 +0545

    Update validator

--- methodconfigs/getRequestConfig.go
+++ methodconfigs/getRequestConfig.go
@@ -1,12 +1,10 @@
 package methodconfigs
 
-type GetRequestConfig struct{
-	Auth bool  `json:"auth"`
-	Query string `json:"query"`
-	QueryParams string `json:"query_params"`
-	Display interface{} `json:"display"`
-	Attachments []string `json:"attachments"`
-	Result  string   `json:"result"`
+type GetRequestConfig struct {
+	Auth        bool        `json:"auth"`
+	Query       string      `json:"query"`
+	QueryParams string      `json:"query_params"`
+	Display     interface{} `json:"display"`
+	Attachments []string    `json:"attachments"`
+	Result      string      `json:"result"`
 }
-
-
