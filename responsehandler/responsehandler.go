7106d6f4faff184d656b3644d30704a9646f5257
anisbhsl <072bct505anish@ioe.edu.np>
2019-11-14 11:08:21 +0545

    JSON response handler :whale2:

--- responsehandler/responsehandler.go
+++ responsehandler/responsehandler.go
@@ -1,1 +1,22 @@
 package responsehandler
+
+import (
+	"net/http"
+	"encoding/json"
+)
+/*
+This package is for sending response to client as per
+schema defined in ```result```.
+
+It takes map[string]interface{} as input, checks its keys as
+per schema in ```result``` and returns a map[string]interface{}
+*/
+
+
+
+func SendJSONResponse(w http.ResponseWriter, response interface{},statusCode int){
+	w.Header().Add("Content-Type","application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response)
+	return
+}
