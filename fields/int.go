880923680f8194c82fb1044d590ddec7c22468fb
Annmayn <ntvirus333@gmail.com>
2019-11-15 16:57:59 +0545

    merged string and int structs

--- fields/int.go
+++ fields/int.go
@@ -10,6 +10,28 @@ type IntStruct struct {
 	errRange   string
 }
 
+//string struct
+type StringStruct struct {
+	Value      string
+	Min_Length int
+	Max_Length int
+	Error_Min  string
+	Error_Max  string
+}
+
+//---------------------------Function definition----------------------
+
+//string validate function
+func (s *StringStruct) Validate(errorMap map[string]string) {
+
+	if len(s.Value) < s.MinLength {
+		errorMap[s.Value] = s.Error_Min
+	} else if len(s.Value) > s.Max_Length {
+		errorMap[s.Value] = s.Max_Length
+	}
+
+}
+
 //Validate : validates int structure
 func (in *IntStruct) Validate(n int, field string, errorMap map[string]string) {
 	if n < in.minValue {
