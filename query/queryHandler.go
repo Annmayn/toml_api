51cb853e90a8b81f6d2dfb383b7ebc67ba39ade3
Annmayn <ntvirus333@gmail.com>
2019-11-20 14:54:13 +0545

    Fixed syntax

--- query/queryHandler.go
+++ query/queryHandler.go
@@ -1,31 +1,33 @@
 package query
 
 import (
+	"fmt"
 	"strings"
 	"toml_api/fileio"
 	"toml_api/getresource"
 )
 
-func QueryHandler(config interface{}, query string, queryParams string, result string, attachments []string) interface{}{
+func QueryHandler(config interface{}, query string, queryParams string, result string, attachments []string) interface{} {
 	//filter query according to resultFields
-	resultFields:=make(map[string]bool)
-	response:=make(map[string]interface{})
+	resultFields := make(map[string]bool)
+	response := make(map[string]interface{})
 
-	result=strings.Replace(result,"$","",1)
-	res:=strings.Split(result,".")
+	result = strings.Replace(result, "$", "", 1)
+	res := strings.Split(result, ".")
 
-	newResultInterface:=getresource.GetResource(config,res[0],res[1])
-	for key,_:=range newResultInterface.(map[string]interface{}){
-		resultFields[key]=true
+	_, newResultInterface := getresource.GetResource(config, res[0], res[1])
+	for key := range newResultInterface.(map[string]interface{}) {
+		resultFields[key] = true
 	}
 
 	//read from db
-	queryResults:=fileio.ReadFromFile()
+	queryResults := fileio.ReadFromFile()
+	fmt.Println(queryResults)
 
 	//filter query results according to resultFields
-	for key,val:=range queryResults.(map[string]interface{}){
-		if _,ok:=resultFields[key];ok{
-			response[key]=val
+	for key, val := range queryResults.(map[string]interface{}) {
+		if _, ok := resultFields[key]; ok {
+			response[key] = val
 		}
 	}
 	//return query result
@@ -33,8 +35,6 @@ func QueryHandler(config interface{}, query string, queryParams string, result s
 
 	/*
 	   TODO: SinkPlugin will receive table name, query params, attachments result
-	 */
-
-
+	*/
 
 }
